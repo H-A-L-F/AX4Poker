45ca6ed9fa219df39f249522e4f07b900acbbf32
Arya Jayadi <arya.jayadi2013@gmail.com>
2024-03-24 14:37:03 +0700

    setup firebase

---
+++ src/data/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-epwfx4QiBhMo2PnczuUaZ9pecnnj5gE",
+  authDomain: "ax4poker.firebaseapp.com",
+  projectId: "ax4poker",
+  storageBucket: "ax4poker.appspot.com",
+  messagingSenderId: "925848927221",
+  appId: "1:925848927221:web:24beffdf75909c87403ebc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db
