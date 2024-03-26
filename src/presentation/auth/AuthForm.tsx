0980a4df9c46319d4aad2ed3365a34b295783dbd
Arya <64455337+H-A-L-F@users.noreply.github.com>
2024-03-26 11:51:48 +0700

    LoginPage odne

--- src/presentation/auth/AuthForm.tsx
+++ src/presentation/auth/AuthForm.tsx
@@ -9,31 +9,40 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
 
-const AuthForm = () => {
+interface AuthFormProps {
+  title: string,
+  link: string,
+  linkText: string,
+}
+
+const AuthForm = ({ title, link, linkText }: AuthFormProps) => {
   return (
     <Card className="w-[350px] bg-background-900 border-none">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>{title}</CardTitle>
+        {/* <CardDescription>Deploy your new project in one-click.</CardDescription> */}
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="name">Username</Label>
+              <Input id="name" placeholder="username" />
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="framework">Password</Label>
+              <Input id="name" placeholder="password" />
             </div>
           </div>
         </form>
+        <div className="mt-2 text-sm">
+          Don't have an account yet? <Link to={"/"+link} className="font-bold">{linkText}</Link>
+        </div>
       </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+      <CardFooter className="flex justify-end">
+        <Button>Submit</Button>
       </CardFooter>
     </Card>
   );
--- src/presentation/auth/LoginPage.tsx
+++ src/presentation/auth/LoginPage.tsx
@@ -3,7 +3,7 @@ import AuthForm from "./AuthForm"
 const LoginPage = () => {
     return (
         <div className="min-w-full min-h-full flex justify-center items-center">
-            <AuthForm />
+            <AuthForm title="Login" link="register" linkText="Register" />
         </div>
     )
 }
