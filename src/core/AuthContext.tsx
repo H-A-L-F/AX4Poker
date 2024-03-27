771651de0df842b6e24d0a62e8bb11514ff2332a
Arya <64455337+H-A-L-F@users.noreply.github.com>
2024-03-27 12:30:59 +0700

    userContext

---
+++ src/core/AuthContext.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, createContext, useContext, useState } from "react";
+
+export type AuthContextType = {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthContextProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
