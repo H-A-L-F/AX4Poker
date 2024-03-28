5087cd6d3d902179784d850cb52f8714ca68f34c
Arya <64455337+H-A-L-F@users.noreply.github.com>
2024-03-28 16:58:25 +0700

    try to make protected route

--- src/core/ApplicationRouter.tsx
+++ src/core/ApplicationRouter.tsx
@@ -4,19 +4,16 @@ import LoginPage from "@/presentation/auth/LoginPage.js";
 import MainLayout from "@/presentation/layout/MainLayout.js";
 import RegisterPage from "@/presentation/auth/RegisterPage.js";
 import ProtectedRoute from "./ProtectedRoute.js";
+import SessionPage from "@/presentation/session/SessionPage.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    
+    element: <ProtectedRoute children={<HomePage />} />,
     children: [
       {
-        path: "/",
-        element: (
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        )
+        path: "/session",
+        element: <SessionPage />
       }
     ]
   },
