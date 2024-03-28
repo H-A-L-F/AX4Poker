9adf137089ee78f0b570b8eb9185f668a16a6e83
Arya <64455337+H-A-L-F@users.noreply.github.com>
2024-03-28 16:52:26 +0700

    hide firebase creds

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-epwfx4QiBhMo2PnczuUaZ9pecnnj5gE",
-  authDomain: "ax4poker.firebaseapp.com",
-  projectId: "ax4poker",
-  storageBucket: "ax4poker.appspot.com",
-  messagingSenderId: "925848927221",
-  appId: "1:925848927221:web:24beffdf75909c87403ebc"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
