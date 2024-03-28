b55fa51c668be98771c3cddb27c815969840c104
Arya <64455337+H-A-L-F@users.noreply.github.com>
2024-03-28 16:40:50 +0700

    Update HomePage.tsx

--- src/presentation/home/HomePage.tsx
+++ src/presentation/home/HomePage.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="min-w-full min-h-full flex flex-col items-center justify-center">
+      <Button>Create Session</Button>
+      <div className="h-4"></div>
+      <Button>Join Session</Button>
+    </div>
   )
 }
 
