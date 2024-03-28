ffa0a4a998edeb33509e6f1a6add2ebc4558606f
Arya <64455337+H-A-L-F@users.noreply.github.com>
2024-03-28 11:50:00 +0700

    navigate to previously targeted page after auth

--- src/presentation/auth/AuthFormViewModel.tsx
+++ src/presentation/auth/AuthFormViewModel.tsx
@@ -2,12 +2,18 @@ import { AuthContextType, useAuthContext } from "@/core/AuthContext"
 import db from "@/data/firebase"
 import { addDoc, collection, doc, getDocs, query, setDoc, where } from "firebase/firestore"
 import { useRef, useState } from "react"
+import { useLocation, useNavigate } from "react-router"
 
 export default function AuthFormViewModel(mode: string) {
     const { setUser } = useAuthContext() as AuthContextType
     const unameRef = useRef<HTMLInputElement>(null)
     const passwordRef = useRef<HTMLInputElement>(null)
     const [errorMsg, setErrorMsg] = useState<string>("")
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const from = location.state?.from?.pathname || "/"
 
     const handleSubmit = () => {
         if(!unameRef.current?.value || !passwordRef.current?.value) {
@@ -29,6 +35,7 @@ export default function AuthFormViewModel(mode: string) {
                     if (data.username == user.uname && data.password == user.password) {
                         console.log(data)
                         setUser(data as User)
+                        navigate(from, { replace: true })
                         break
                     }
                 }
@@ -37,7 +44,8 @@ export default function AuthFormViewModel(mode: string) {
             })
         } else if (mode == "Register") {
             addDoc(userRef, { username: user.uname, password: user.password }).then((docRef) => {
-                setUser({ username: user.uname, password: user.password });
+                setUser({ username: user.uname, password: user.password })
+                navigate(from, { replace: true })
             }).catch((error) => {
                 console.error("Error registering user: ", error);
             });
